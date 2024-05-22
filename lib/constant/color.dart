5518c0b2b0db5041aa618aa6e503734475b1cdc1
Dimas Salahuddin <dimassalah10@gmail.com>
2024-05-22 21:31:37 +0700

    intial commit

---
+++ lib/constant/color.dart
@@ -0,0 +1,40 @@
+part of 'constant.dart';
+
+class Primary {
+  static const Color mainColor = Color(0xFF08B6D5);
+  static const Color darker = Color(0xFF0E7491);
+  static const Color lighter = Color(0xFF9BEDFC);
+  static const Color subtle = Color(0xFF90C5CE);
+}
+
+class Error {
+  static const Color mainColor = Color(0xFFFF3B3B);
+  static const Color darker = Color(0xFFE53535);
+  static const Color lighter = Color(0xFFFF5C5C);
+  static const Color subtle = Color(0xFFFF8080);
+}
+
+class Warning {
+  static const Color mainColor = Color(0xFFFFCC00);
+  static const Color darker = Color(0xFFE5B800);
+  static const Color lighter = Color(0xFFFDDD48);
+  static const Color subtle = Color(0xFFFDED72);
+}
+
+class Success {
+  static const Color mainColor = Color(0xFF06C270);
+  static const Color darker = Color(0xFF05A660);
+  static const Color lighter = Color(0xFF39D98A);
+  static const Color subtle = Color(0xFF56EBA0);
+}
+
+class Neutral {
+  static const Color dark1 = Color(0xFF1A1A1A);
+  static const Color dark2 = Color(0xFF5B5F67);
+  static const Color dark3 = Color(0xFF8F90A6);
+  static const Color dark4 = Color(0xFFC7C9D9);
+  static const Color light1   = Color(0xFFDDE5E9);
+  static const Color light2 = Color(0xFFDDE5E9);
+  static const Color light3 = Color(0xFFFAFAFC);
+  static const Color light4 = Color(0xFFFDFDFF);
+}
