0b3220755426b52724c3d66882843c187bf67f1d
“k47tan” <hashby.alhadi@gmail.com>
2024-05-25 14:49:02 +0700

    ✨ feat: Add new routes for meditation, music, article, story, and wishlist views

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,7 +1,17 @@
 import 'package:get/get.dart';
 
+import '../modules/article/bindings/article_binding.dart';
+import '../modules/article/views/article_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/meditation/bindings/meditation_binding.dart';
+import '../modules/meditation/views/meditation_view.dart';
+import '../modules/music/bindings/music_binding.dart';
+import '../modules/music/views/music_view.dart';
+import '../modules/story/bindings/story_binding.dart';
+import '../modules/story/views/story_view.dart';
+import '../modules/wishlist/bindings/wishlist_binding.dart';
+import '../modules/wishlist/views/wishlist_view.dart';
 
 part 'app_routes.dart';
 
@@ -15,6 +25,31 @@ class AppPages {
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.MEDITATION,
+      page: () => const MeditationView(),
+      binding: MeditationBinding(),
+    ),
+    GetPage(
+      name: _Paths.MUSIC,
+      page: () => const MusicView(),
+      binding: MusicBinding(),
+    ),
+    GetPage(
+      name: _Paths.ARTICLE,
+      page: () => const ArticleView(),
+      binding: ArticleBinding(),
+    ),
+    GetPage(
+      name: _Paths.STORY,
+      page: () => const StoryView(),
+      binding: StoryBinding(),
+    ),
+    GetPage(
+      name: _Paths.WISHLIST,
+      page: () => const WishlistView(),
+      binding: WishlistBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -4,9 +4,19 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
+  static const MEDITATION = _Paths.MEDITATION;
+  static const MUSIC = _Paths.MUSIC;
+  static const ARTICLE = _Paths.ARTICLE;
+  static const STORY = _Paths.STORY;
+  static const WISHLIST = _Paths.WISHLIST;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
+  static const MEDITATION = '/meditation';
+  static const MUSIC = '/music';
+  static const ARTICLE = '/article';
+  static const STORY = '/story';
+  static const WISHLIST = '/wishlist';
 }
