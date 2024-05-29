729c338d3898a2d2c3bf565dfc88cac5c438413d
Muhammad Syabdewa <syabdewa02@gmail.com>
2024-05-29 19:53:15 +0700

    refactor: Add email field and validation to AuthController

--- lib/app/modules/auth/controllers/auth_controller.dart
+++ lib/app/modules/auth/controllers/auth_controller.dart
@@ -1,23 +1,21 @@
 import 'package:get/get.dart';
 
-class AuthController extends GetxController {
+import '../mixins/forget_password_mixin.dart';
+import '../mixins/verification_code_mixin.dart';
+
+class AuthController extends GetxController
+    with VerificationCodeMixin, ForgetPasswordMixin {
   RxString username = ''.obs;
+  RxString email = ''.obs;
   RxString password = ''.obs;
-  RxString icon = ''.obs;
+  RxString confirmPassword = ''.obs;
   RxBool obscureText = true.obs;
+  RxString icon = ''.obs;
 
   validatePassword(String? pwd) {
     if (pwd!.length < 6) {
       return 'Password minimal 6 karakter';
     }
     return null;
-  }
-
-  void setUsername(String value) {
-    username.value = value;
-  }
-
-  void setPassword(String value) {
-    password.value = value;
   }
 }
