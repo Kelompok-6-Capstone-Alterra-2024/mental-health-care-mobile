c3202f5d324fe1bde23d02735ce99194854f216a
Muhammad Syabdewa <syabdewa02@gmail.com>
2024-05-29 19:54:26 +0700

    refactor: Add ForgetPasswordView for handling password reset functionality

---
+++ lib/app/modules/auth/views/forget_password_view.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+import 'package:get/get.dart';
+
+import '../../../../constant/constant.dart';
+import '../../../../utils/global_components/main_button.dart';
+import '../../../../utils/global_components/text_input_field.dart';
+import '../../../routes/app_pages.dart';
+import '../controllers/auth_controller.dart';
+
+class ForgetPasswordView extends GetView<AuthController> {
+  const ForgetPasswordView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Gap(100),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 22),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Lupa Password?',
+                  style:
+                      semiBold.copyWith(fontSize: 24, color: Primary.mainColor),
+                ),
+              ),
+            ),
+            const Gap(16),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 22),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Tidak Perlu Khawatir, Kami Akan Membantu Anda Mengatur Ulang Kata Sandi Anda',
+                  style: regular.copyWith(fontSize: 16, color: Neutral.dark3),
+                ),
+              ),
+            ),
+            const Gap(24),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 22),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Email',
+                  style: medium.copyWith(fontSize: 16, color: Neutral.dark1),
+                ),
+              ),
+            ),
+            const Gap(10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 22),
+              child: InputField(
+                title: 'Ketikkan Email',
+                onChanged: controller.username.call,
+              ),
+            ),
+            const Gap(32),
+            MainButton(
+              label: 'Kirim Code',
+              onTap: () {
+                // Logic Here
+                Get.toNamed(Routes.VERIFICATION_CODE);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
