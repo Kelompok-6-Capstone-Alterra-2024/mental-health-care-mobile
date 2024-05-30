82bbf99bda868a207ce5cc3fafb653e7b113ddf0
Muhammad Syabdewa <syabdewa02@gmail.com>
2024-05-30 20:30:22 +0700

    refactor: Update VerificationCodeView layout and styles, and handle verification code input

--- lib/app/modules/auth/views/register_view.dart
+++ lib/app/modules/auth/views/register_view.dart
@@ -18,201 +18,208 @@ class RegisterView extends GetView<AuthController> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const Gap(100),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 22),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Mulai Perjalanan Anda',
-                  style:
-                      semiBold.copyWith(fontSize: 24, color: Primary.mainColor),
-                ),
-              ),
-            ),
-            const Gap(16),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 22),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Daftar untuk Memulai Perjalanan Menuju Kesehatan Mental yang Lebih Baik',
-                  style: regular.copyWith(fontSize: 16, color: Neutral.dark3),
-                ),
-              ),
-            ),
-            const Gap(40),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 22),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Email',
-                  style: medium.copyWith(fontSize: 16, color: Neutral.dark1),
-                ),
-              ),
-            ),
-            const Gap(10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 22),
-              child: InputField(
-                title: 'Ketikkan Email',
-                onChanged: controller.email.call,
-              ),
-            ),
-            const Gap(16),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 22),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Username',
-                  style: medium.copyWith(fontSize: 16, color: Neutral.dark1),
-                ),
-              ),
-            ),
-            const Gap(10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 22),
-              child: InputField(
-                title: 'Ketikkan Username',
-                onChanged: controller.username.call,
-              ),
-            ),
-            const Gap(16),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 22),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Password',
-                  style: medium.copyWith(fontSize: 16, color: Neutral.dark1),
-                ),
-              ),
-            ),
-            const Gap(10),
-            Obx(
-              () => Padding(
+        child: Form(
+          key: controller.formKey,
+          child: Column(
+            children: [
+              const Gap(100),
+              Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 22),
-                child: InputField(
-                  title: 'Ketikkan Password',
-                  onChanged: controller.password.call,
-                  obscureText: controller.obscureText.value,
-                  validator: (pwd) => controller.validatePassword(pwd),
-                  icon: GestureDetector(
-                    onTap: () {
-                      controller.obscureText.value =
-                          !controller.obscureText.value;
-                    },
-                    child: SizedBox(
-                      width: 10,
-                      height: 20,
-                      child: SvgPicture.asset(
-                        controller.obscureText.value
-                            ? 'assets/icons/eye-closed.svg'
-                            : 'assets/icons/eye-open.svg',
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            const Gap(16),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 22),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Konfirmasi Password',
-                  style: medium.copyWith(fontSize: 16, color: Neutral.dark1),
-                ),
-              ),
-            ),
-            const Gap(10),
-            Obx(
-              () => Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 22),
-                child: InputField(
-                  title: 'Ketikkan konfirmasi password',
-                  onChanged: controller.confirmPassword.call,
-                  obscureText: controller.obscureText.value,
-                  validator: (pwd) => controller.validatePassword(pwd),
-                  icon: GestureDetector(
-                    onTap: () {
-                      controller.obscureText.value =
-                          !controller.obscureText.value;
-                    },
-                    child: SizedBox(
-                      width: 10,
-                      height: 20,
-                      child: SvgPicture.asset(
-                        controller.obscureText.value
-                            ? 'assets/icons/eye-closed.svg'
-                            : 'assets/icons/eye-open.svg',
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            const Gap(32),
-            MainButton(
-              label: 'Daftar',
-              onTap: () {
-                // Logic Daftar
-              },
-            ),
-            const Gap(32),
-            Text(
-              'Atau masuk dengan',
-              style: medium.copyWith(fontSize: 16, color: Neutral.dark3),
-            ),
-            const Gap(32),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AuthButton(
-                  svgAssetPath: 'assets/icons/google.svg',
-                  label: 'Google',
-                  onTap: () {
-                    // Implement Google Sign In
-                    log('Google Sign In button tapped');
-                  },
-                ),
-                const Gap(20),
-                AuthButton(
-                  svgAssetPath: 'assets/icons/facebook.svg',
-                  label: 'Facebook',
-                  onTap: () {
-                    // Implement Facebook Sign In
-                    log('Facebook Sign In button tapped');
-                  },
-                ),
-              ],
-            ),
-            const Gap(35),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Sudah punya akun? ',
-                  style: medium.copyWith(fontSize: 16, color: Neutral.dark1),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Get.offNamed(Routes.LOGIN);
-                  },
+                child: Align(
+                  alignment: Alignment.centerLeft,
                   child: Text(
-                    'Masuk',
-                    style:
-                        bold.copyWith(fontSize: 16, color: Primary.mainColor),
+                    'Mulai Perjalanan Anda',
+                    style: semiBold.copyWith(
+                        fontSize: 24, color: Primary.mainColor),
                   ),
                 ),
-              ],
-            ),
-          ],
+              ),
+              const Gap(16),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 22),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Daftar untuk Memulai Perjalanan Menuju Kesehatan Mental yang Lebih Baik',
+                    style: regular.copyWith(fontSize: 16, color: Neutral.dark3),
+                  ),
+                ),
+              ),
+              const Gap(40),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 22),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Email',
+                    style: medium.copyWith(fontSize: 16, color: Neutral.dark1),
+                  ),
+                ),
+              ),
+              const Gap(10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 22),
+                child: InputField(
+                  title: 'Ketikkan Email',
+                  validator: (email) => controller.validateEmail(email),
+                  onChanged: controller.setEmail,
+                ),
+              ),
+              const Gap(16),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 22),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Username',
+                    style: medium.copyWith(fontSize: 16, color: Neutral.dark1),
+                  ),
+                ),
+              ),
+              const Gap(10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 22),
+                child: InputField(
+                  title: 'Ketikkan Username',
+                  validator: (username) =>
+                      controller.validateUsername(username),
+                  onChanged: controller.setUserName,
+                ),
+              ),
+              const Gap(16),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 22),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Password',
+                    style: medium.copyWith(fontSize: 16, color: Neutral.dark1),
+                  ),
+                ),
+              ),
+              const Gap(10),
+              Obx(
+                () => Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 22),
+                  child: InputField(
+                    title: 'Ketikkan Password',
+                    onChanged: controller.setPassword,
+                    obscureText: controller.obscureText.value,
+                    validator: (pwd) => controller.validatePassword(pwd),
+                    icon: GestureDetector(
+                      onTap: () {
+                        controller.obscureText.value =
+                            !controller.obscureText.value;
+                      },
+                      child: SizedBox(
+                        width: 10,
+                        height: 20,
+                        child: SvgPicture.asset(
+                          controller.obscureText.value
+                              ? 'assets/icons/eye-closed.svg'
+                              : 'assets/icons/eye-open.svg',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const Gap(16),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 22),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Konfirmasi Password',
+                    style: medium.copyWith(fontSize: 16, color: Neutral.dark1),
+                  ),
+                ),
+              ),
+              const Gap(10),
+              Obx(
+                () => Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 22),
+                  child: InputField(
+                    title: 'Ketikkan konfirmasi password',
+                    onChanged: controller.setConfirmPassword,
+                    obscureText: controller.obscureText.value,
+                    validator: (pwd) => controller.validateConfirmPassword(pwd),
+                    icon: GestureDetector(
+                      onTap: () {
+                        controller.obscureText.value =
+                            !controller.obscureText.value;
+                      },
+                      child: SizedBox(
+                        width: 10,
+                        height: 20,
+                        child: SvgPicture.asset(
+                          controller.obscureText.value
+                              ? 'assets/icons/eye-closed.svg'
+                              : 'assets/icons/eye-open.svg',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const Gap(32),
+              MainButton(
+                label: 'Daftar',
+                onTap: () {
+                  // Logic Daftar
+                  // controller.register();
+                },
+              ),
+              const Gap(32),
+              Text(
+                'Atau masuk dengan',
+                style: medium.copyWith(fontSize: 16, color: Neutral.dark3),
+              ),
+              const Gap(32),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AuthButton(
+                    svgAssetPath: 'assets/icons/google.svg',
+                    label: 'Google',
+                    onTap: () {
+                      // Implement Google Sign In
+                      log('Google Sign In button tapped');
+                    },
+                  ),
+                  const Gap(20),
+                  AuthButton(
+                    svgAssetPath: 'assets/icons/facebook.svg',
+                    label: 'Facebook',
+                    onTap: () {
+                      // Implement Facebook Sign In
+                      log('Facebook Sign In button tapped');
+                    },
+                  ),
+                ],
+              ),
+              const Gap(35),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Sudah punya akun? ',
+                    style: medium.copyWith(fontSize: 16, color: Neutral.dark1),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Get.offNamed(Routes.LOGIN);
+                    },
+                    child: Text(
+                      'Masuk',
+                      style:
+                          bold.copyWith(fontSize: 16, color: Primary.mainColor),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/app/modules/auth/views/verification_code_view.dart
+++ lib/app/modules/auth/views/verification_code_view.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
 import 'package:gap/gap.dart';
@@ -14,87 +16,85 @@ class VerificationCodeView extends GetView<AuthController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const Gap(100),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 22),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Verifikasi Kode Anda',
-                  style:
-                      semiBold.copyWith(fontSize: 24, color: Primary.mainColor),
-                ),
+      body: Column(
+        children: [
+          const Gap(100),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 22),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Verifikasi Kode Anda',
+                style:
+                    semiBold.copyWith(fontSize: 24, color: Primary.mainColor),
               ),
             ),
-            const Gap(16),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 22),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Masukkan Kode Verifikasi yang Kami Kirim ke Email Anda',
-                  style: regular.copyWith(fontSize: 16, color: Neutral.dark3),
-                ),
+          ),
+          const Gap(16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 22),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Masukkan Kode Verifikasi yang Kami Kirim ke Email Anda',
+                style: regular.copyWith(fontSize: 16, color: Neutral.dark3),
               ),
             ),
-            const Gap(24),
-            OtpTextField(
-              fieldWidth: 64,
-              fieldHeight: 64,
-              numberOfFields: 5,
-              borderColor: Neutral.dark4,
-              showFieldAsBox: true,
-              focusedBorderColor: Primary.mainColor,
-              cursorColor: Primary.mainColor,
-              textStyle: regular.copyWith(fontSize: 16, color: Neutral.dark1),
-              onCodeChanged: (String code) {
-                //handle validation or checks here
-              },
-              onSubmit: (String verificationCode) {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: const Text("Verification Code"),
-                      content: Text('Code entered is $verificationCode'),
-                    );
-                  },
-                );
-              },
-            ),
-            const Gap(32),
-            MainButton(
-              label: 'Kirim Code',
-              onTap: () {
-                // Logic Here
-                Get.toNamed(Routes.NEW_PASSWORD);
-              },
-            ),
-            const Gap(16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Belum menerima kode? ',
-                  style: medium.copyWith(fontSize: 16, color: Neutral.dark1),
+          ),
+          const Gap(24),
+          OtpTextField(
+            fieldWidth: 64,
+            fieldHeight: 64,
+            numberOfFields: 5,
+            borderColor: Neutral.dark4,
+            showFieldAsBox: true,
+            focusedBorderColor: Primary.mainColor,
+            cursorColor: Primary.mainColor,
+            textStyle: regular.copyWith(fontSize: 16, color: Neutral.dark1),
+            onCodeChanged: (String code) {
+              //handle validation or checks here
+            },
+            onSubmit: (String verificationCode) {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text("Verification Code"),
+                    content: Text('Code entered is $verificationCode'),
+                  );
+                },
+              );
+            },
+          ),
+          const Gap(32),
+          MainButton(
+            label: 'Kirim Code',
+            onTap: () {
+              // Logic Here
+              // Get.toNamed(Routes.NEW_PASSWORD);
+              log('Verification Code View: Send Code Button Pressed');
+            },
+          ),
+          const Gap(16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Belum menerima kode? ',
+                style: medium.copyWith(fontSize: 16, color: Neutral.dark1),
+              ),
+              GestureDetector(
+                onTap: () {
+                  // Logic Here
+                },
+                child: Text(
+                  'Kirim Ulang',
+                  style: bold.copyWith(fontSize: 16, color: Primary.mainColor),
                 ),
-                GestureDetector(
-                  onTap: () {
-                    // Logic Here
-                  },
-                  child: Text(
-                    'Kirim Ulang',
-                    style:
-                        bold.copyWith(fontSize: 16, color: Primary.mainColor),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
