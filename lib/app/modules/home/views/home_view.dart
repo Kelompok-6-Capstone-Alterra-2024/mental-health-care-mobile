d3e09bf753ab5ea15c9d956a663762e81ccc3c83
Dimas Salahuddin <dimassalah10@gmail.com>
2024-06-04 18:09:35 +0700

    fix: fix error when tap navigation in home view

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -14,6 +14,7 @@ class HomeView extends GetView<HomeController> {
   const HomeView({super.key});
   @override
   Widget build(BuildContext context) {
+    controller;
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
