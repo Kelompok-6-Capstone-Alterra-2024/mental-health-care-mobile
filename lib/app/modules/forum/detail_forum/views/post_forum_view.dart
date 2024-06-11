40cd3ea973bf60961c67506348f1e573a6a55cd5
Muhammad Syabdewa <syabdewa02@gmail.com>
2024-06-11 21:38:12 +0700

    feat: Update post forum view with new title and posting button

--- lib/app/modules/forum/detail_forum/views/post_forum_view.dart
+++ lib/app/modules/forum/detail_forum/views/post_forum_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
 
 import 'package:get/get.dart';
 
+import '../../../../../constant/constant.dart';
 import '../controllers/detail_forum_controller.dart';
 
 class PostForumView extends GetView<DetailForumController> {
@@ -11,7 +13,10 @@ class PostForumView extends GetView<DetailForumController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('PostForumView'),
+        title: Text(
+          'Posting Diskusi',
+          style: medium.copyWith(fontSize: 16, color: Primary.darker),
+        ),
         leading: IconButton(
           icon: SvgPicture.asset(
             'assets/icons/Chevron.svg',
@@ -22,11 +27,47 @@ class PostForumView extends GetView<DetailForumController> {
           },
         ),
         centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: GestureDetector(
+              onTap: () {},
+              child: Container(
+                alignment: Alignment.center,
+                height: 27,
+                width: 79,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Primary.mainColor,
+                ),
+                child: Text(
+                  'Posting',
+                  style: medium.copyWith(fontSize: 12, color: Neutral.light4),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text(
-          'PostForumView is working',
-          style: TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 26),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const TextField(
+              decoration: InputDecoration(
+                hintText: 'Apa yang ingin anda bicarakan?',
+                border: InputBorder.none,
+              ),
+            ),
+            const Gap(5),
+            SvgPicture.asset(
+              'assets/icons/upload_photo.svg',
+              width: 24,
+              height: 24,
+            ),
+          ],
         ),
       ),
     );
