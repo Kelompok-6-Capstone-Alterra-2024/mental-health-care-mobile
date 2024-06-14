825a4f264d3a9ed7b375e8ad005c5d85cef67d5e
Muhammad Syabdewa <syabdewa02@gmail.com>
2024-06-14 21:43:34 +0700

    feat: Add RecommendationForumModel and RecommendationForumService

---
+++ lib/app/modules/forum/data/services/recommendation_forum_service.dart
@@ -0,0 +1,40 @@
+import 'package:dio/dio.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:logger/logger.dart';
+
+import '../models/recommendation_forum_model.dart';
+
+class RecommendationForumService {
+  final Dio _dio = Dio();
+  final logger = Logger();
+  final String token = GetStorage().read('token') ?? '';
+  final baseUrl = 'https://dev-capstone.practiceproject.tech/v1/users';
+
+  Future<RecommendationForumModel> getRecommendationForums() async {
+    try {
+      final url = '$baseUrl/forums/recommendation?page=1&limit=10';
+      logger.i('Request URL: $url');
+
+      final response = await _dio.get(
+        url,
+        options: Options(
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $token',
+          },
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        logger.i('Response data: ${response.data}');
+        return RecommendationForumModel.fromJson(response.data);
+      } else {
+        throw Exception(
+            'Failed to load recommendation forums with status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      logger.e('Error loading recommendation forums: $e');
+      throw Exception('Failed to load recommendation forums: $e');
+    }
+  }
+}
