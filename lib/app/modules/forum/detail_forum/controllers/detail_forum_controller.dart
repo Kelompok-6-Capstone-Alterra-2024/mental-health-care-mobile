ccf729c067f5d341320f7e5cf0f05c0e53d39ec4
Muhammad Syabdewa <syabdewa02@gmail.com>
2024-06-14 23:44:23 +0700

    feat: Implement fetchForumById method in DetailForumController

--- lib/app/modules/forum/detail_forum/controllers/detail_forum_controller.dart
+++ lib/app/modules/forum/detail_forum/controllers/detail_forum_controller.dart
@@ -1,23 +1,25 @@
 import 'package:get/get.dart';
 
-class DetailForumController extends GetxController {
-  //TODO: Implement DetailForumController
+import '../data/models/forum_by_id_model.dart';
+import '../data/services/forum_by_id_service.dart';
 
-  final count = 0.obs;
+class DetailForumController extends GetxController {
+  final ForumByIdService _forumByIdService = ForumByIdService();
+
+  Rx<ForumByIdModel?> forumById = Rx<ForumByIdModel?>(null);
+
   @override
   void onInit() {
     super.onInit();
+    fetchForumById(2);
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  void fetchForumById(int forumId) async {
+    try {
+      final forum = await _forumByIdService.getForumById(forumId);
+      forumById.value = forum;
+    } catch (e) {
+      print('Error fetching forum by id: $e');
+    }
   }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/forum/detail_forum/views/detail_forum_view.dart
+++ lib/app/modules/forum/detail_forum/views/detail_forum_view.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
-
 import 'package:get/get.dart';
 import 'package:mindease/app/modules/forum/detail_forum/views/components/post_card.dart';
-
 import '../../../../../constant/constant.dart';
 import '../../../../routes/app_pages.dart';
 import '../controllers/detail_forum_controller.dart';
 
 class DetailForumView extends GetView<DetailForumController> {
   const DetailForumView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Neutral.light2,
       appBar: AppBar(
-        title: Text(
-          'TalkLife',
-          style: medium.copyWith(fontSize: 16, color: Primary.darker),
-        ),
+        title: Obx(() => Text(
+              controller.forumById.value?.data.name ?? 'Forum Detail',
+              style: medium.copyWith(fontSize: 16, color: Primary.darker),
+            )),
         leading: IconButton(
           icon: SvgPicture.asset(
             'assets/icons/Chevron.svg',
@@ -32,142 +31,92 @@ class DetailForumView extends GetView<DetailForumController> {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container( // header top card
-              padding: const EdgeInsets.symmetric(horizontal: 44, vertical: 22),
-              decoration: BoxDecoration(
-                color: Neutral.light4,
-                borderRadius: BorderRadius.circular(8),
-                boxShadow: [
-                  blur4,
-                  blur8,
-                ],
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        width: 72,
-                        height: 72,
-                        decoration: BoxDecoration(
-                          image: const DecorationImage(
-                            image: AssetImage(
-                                'assets/images/my_forum_cover.png'),
-                            fit: BoxFit.cover,
-                          ),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                      const Gap(20),
-                      Text(
-                        'TalkLife',
-                        style: semiBold.copyWith(
-                            fontSize: 24, color: Neutral.dark1),
-                      ),
-                    ],
-                  ),
-                  const Gap(20),
-                  Text(
-                    textAlign: TextAlign.justify,
-                    'Forum yang membantu pengguna dari seluruh indonesia  berbagi pengalaman, mendukung satu sama lain, dan mengatasi tantangan kesehatan mental bersama',
-                    style: regular.copyWith(
-                      fontSize: 12,
-                      color: Neutral.dark2,
-                    ),
-                  ),
-                  const Gap(40),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 40),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Obx(() {
+          final forum = controller.forumById.value?.data;
+          if (forum == null) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                decoration: BoxDecoration(
+                  color: Neutral.light4,
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    blur4,
+                    blur8,
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
                       children: [
-                        Column(
-                          children: [
-                            SvgPicture.asset(
-                              'assets/icons/member.svg',
-                              width: 24,
-                              height: 24,
+                        Container(
+                          width: 72,
+                          height: 72,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(forum.imageUrl),
+                              fit: BoxFit.cover,
                             ),
-                            const Gap(6),
-                            Text(
-                              'Anggota',
-                              style: medium.copyWith(
-                                  fontSize: 12, color: Neutral.dark3),
-                            ),
-                          ],
+                            borderRadius: BorderRadius.circular(8),
+                          ),
                         ),
                         const Gap(20),
-                        Column(
-                          children: [
-                            SvgPicture.asset(
-                              'assets/icons/share.svg',
-                              width: 24,
-                              height: 24,
-                            ),
-                            const Gap(6),
-                            Text(
-                              'Bagi',
-                              style: medium.copyWith(
-                                  fontSize: 12, color: Neutral.dark3),
-                            ),
-                          ],
-                        ),
-                        const Gap(20),
-                        Column(
-                          children: [
-                            SvgPicture.asset(
-                              'assets/icons/quit.svg',
-                              width: 24,
-                              height: 24,
-                            ),
-                            const Gap(6),
-                            Text(
-                              'Keluar',
-                              style: medium.copyWith(
-                                  fontSize: 12, color: Neutral.dark3),
-                            ),
-                          ],
+                        Text(
+                          forum.name,
+                          style: semiBold.copyWith(
+                              fontSize: 24, color: Neutral.dark1),
                         ),
                       ],
                     ),
-                  ),
-                ],
+                    const Gap(20),
+                    Text(
+                      forum.description,
+                      style:
+                          regular.copyWith(fontSize: 12, color: Neutral.dark2),
+                      textAlign: TextAlign.justify,
+                    ),
+                    const Gap(40),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          _buildActionItem(
+                              'Anggota', 'assets/icons/member.svg'),
+                          _buildActionItem('Bagi', 'assets/icons/share.svg'),
+                          _buildActionItem('Keluar', 'assets/icons/quit.svg'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const Gap(32),
-            ListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: 3,
-              itemBuilder: (context, index) {
-                return const PostCard();
-              },
-            )
-          ],
-        ),
+              const Gap(32),
+              ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: 3,
+                itemBuilder: (context, index) {
+                  return const PostCard();
+                },
+              ),
+            ],
+          );
+        }),
       ),
-      floatingActionButton: Container(
-        margin: const EdgeInsets.only(bottom: 10, right: 16),
-        padding: const EdgeInsets.symmetric(vertical: 4),
-        height: 53,
-        width: 125,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(24),
-          color: Primary.mainColor,
-        ),
-        child: Row(
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Get.toNamed(Routes.POST_FORUM);
+        },
+        label: Row(
           children: [
-            FloatingActionButton(
-              shape: const CircleBorder(),
-              backgroundColor: Colors.white,
-              elevation: 0,
-              onPressed: () {
-                Get.toNamed(Routes.POST_FORUM);
-              },
-              child: SvgPicture.asset('assets/icons/plus.svg'),
-            ),
+            SvgPicture.asset('assets/icons/plus.svg'),
             const Gap(8),
             Text(
               'Post',
@@ -175,7 +124,26 @@ class DetailForumView extends GetView<DetailForumController> {
             ),
           ],
         ),
+        backgroundColor: Primary.mainColor,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
       ),
+    );
+  }
+
+  Widget _buildActionItem(String title, String iconPath) {
+    return Column(
+      children: [
+        SvgPicture.asset(
+          iconPath,
+          width: 24,
+          height: 24,
+        ),
+        const Gap(6),
+        Text(
+          title,
+          style: medium.copyWith(fontSize: 12, color: Neutral.dark3),
+        ),
+      ],
     );
   }
 }
